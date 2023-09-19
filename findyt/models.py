e60586f70f157aa49bd2ff3785234ee82a6eed5b
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-19 07:40:26 +0700

    feat: create models for findyt

--- findyt/models.py
+++ findyt/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=250)
+    playlist_id = models.CharField(max_length=250)
+    thumbnail_url = models.CharField(max_length=250)
+    description = models.TextField()
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=250)
+    views = models.IntegerField()
+    likes = models.IntegerField()
+    youtube_id = models.CharField(max_length=250)
+    date_published = models.DateField()
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
