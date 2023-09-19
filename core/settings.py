4f572da2f6d8ed364e504dc7df944679e850b17d
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-19 10:39:31 +0700

    add youtube api key variable

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
